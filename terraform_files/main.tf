1f01cde12203c713b2260ea03bfe9ae7341b986b
Udhai Ram <udhairam@Udhais-Mac-mini.local>
2026-02-08 18:51:21 -0800

    changed and modified files to test terraform through VCS driven workflow

---
+++ terraform_files/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = var.bucket_name
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.demo.bucket
+}
