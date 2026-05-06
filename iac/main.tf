8d513f87d5f1da97c8b36c894ac5fc324483e682
Udhai Ram <udhairam@Udhais-Mac-mini.local>
2026-05-05 20:55:14 -0700

    dummy commit

--- iac/main.tf
+++ iac/main.tf
@@ -79,15 +79,3 @@ resource "aws_security_group" "web_sg" {
 resource "aws_s3_bucket" "sample" {
   bucket = var.bucket_name
 }
-
-resource "aws_instance" "web" {
-  ami                    = "ami-0c1fe732b5494dc14"
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "web-instance"
-  }
-}
